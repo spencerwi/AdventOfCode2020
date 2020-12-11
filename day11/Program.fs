d8015b4dc6976eae96bfe7133dc4056d97efe492
Spencer Williams <spencerwi@gmail.com>
2020-12-11 16:03:54 -0500

    Day11: Add comments/docs

--- day11/Program.fs
+++ day11/Program.fs
@@ -1,8 +1,9 @@
 open System
 
 module Seating = begin
-    type Grid = char [,]
+    type Grid = char [,] // this is F# for "a 2d array of char"
 
+    /// A convenience "is this in bounds" checker method.
     let doesSeatExist seatingGrid (row, col) =
         (
             row >= 0 && row < (Array2D.length1 seatingGrid)
@@ -14,6 +15,10 @@ module Seating = begin
         | '#' | 'L' -> true
         | _ -> false
 
+    /// <returns>
+    /// All immediately-adjacent locations for a given (row, col) point 
+    /// that are actually in-bounds on the grid.
+    /// </returns>
     let getAdjacentSpots (row, col) seatingGrid =
         seq {
             for i in (row - 1) .. (row + 1) do
@@ -23,12 +28,20 @@ module Seating = begin
                             yield (i, j)
         }
 
+    /// <returns>
+    /// The number of immediately-adjacent spots that are occupied seats.
+    /// </returns>
     let countAdjacentOccupiedSeats (seatingGrid : Grid) (row, col) =
         getAdjacentSpots (row, col) seatingGrid
         |> Seq.map (fun (row, col) -> seatingGrid.[row, col])
         |> Seq.filter (fun spot -> spot = '#')
         |> Seq.length
 
+    /// <returns>
+    /// All the seats (occupied *or* unoccupied) that are in straight-line 
+    /// line-of-sight from the given location in all 8 directions, taking 
+    /// into account that some "sightlines" will *not* have contain a seat.
+    /// </returns>
     let getVisibleSeats (row, col) seatingGrid =
         let directionsToSearch = [
             (-1, -1); (-1, 0); (-1, 1);
@@ -51,12 +64,22 @@ module Seating = begin
         |> Seq.filter Option.isSome
         |> Seq.map Option.get
 
+    /// <returns>
+    /// The number of seats visible from a given spot (see getVisibleSeats) that 
+    /// are occupied.
+    /// </returns>
     let countVisibleOccupiedSeats (seatingGrid : Grid) (row, col) =
         getVisibleSeats (row, col) seatingGrid
         |> Seq.map (fun (row, col) -> seatingGrid.[row, col])
         |> Seq.filter (fun spot -> spot = '#')
         |> Seq.length
 
+    /// <summary>
+    /// Steps forward in time one "step" based on the rules in Part 1 of the puzzle.
+    /// </summary>
+    /// <returns>
+    /// A new Grid that represents the state of the world after 1 time unit.
+    /// </returns>
     let step1 (grid : Grid) : Grid =
         grid
         |> Array2D.mapi (fun row col spot ->
@@ -66,7 +89,12 @@ module Seating = begin
             | '#' when adjacentOccupiedSeats >= 4 -> 'L'
             | _ -> spot 
         )
-
+    /// <summary>
+    /// Steps forward in time one "step" based on the rules in Part 2 of the puzzle.
+    /// </summary>
+    /// <returns>
+    /// A new Grid that represents the state of the world after 1 time unit.
+    /// </returns>
     let step2 (grid : Grid) : Grid =
         grid
         |> Array2D.mapi (fun row col spot ->
@@ -77,12 +105,23 @@ module Seating = begin
             | _ -> spot 
         )
 
+    /// <returns>
+    /// All occupied seats anywhere in the given Grid.
+    /// </returns>
     let countAllOccupiedSeats (seatingGrid : Grid) =
         Seq.length (seq {
             for spot in Seq.cast<char> seatingGrid do
                 if spot = '#' then yield spot
         })
 
+    /// <summary>
+    /// Continuously steps through time, starting with the provided Grid and
+    /// using the provided stepper function for each step, until a "stable point"
+    /// where nothing in the Grid changes anymore is found.
+    /// </summary>
+    /// <returns>
+    /// A Grid that represents the world at at that stable point.
+    /// </returns>
     let runUntilStable (seatingGrid : Grid) (stepper : Grid -> Grid) =
         let mutable currentGrid = seatingGrid in
         let mutable stablePointFound = false in
