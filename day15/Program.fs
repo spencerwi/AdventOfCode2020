4c745dfd5e45b91c8d16710c1588ac38f848da20
Spencer Williams <spencerwi@gmail.com>
2020-12-16 16:22:46 -0500

    Day15: Part 2 solution too

--- day15/Program.fs
+++ day15/Program.fs
@@ -6,38 +6,61 @@ let arrayLast (arr : 'a array) : 'a =
     else
         arr.[arr.Length - 1]
 
-let arrayDropLast (arr : 'a array) : 'a array =
-    if Array.isEmpty arr then arr
-    else
-        arr.[0..(arr.Length - 2)]
 
-
-let part1 (startingNumbers : int array) =
-    let speak (alreadySpokenNumbers : int array) =
-        let lastNumber = arrayLast alreadySpokenNumbers in
-        let numbersUpToThatPoint = arrayDropLast alreadySpokenNumbers in
+let solve (startingNumbers : int array) =
+    let mutable occurrencesMap : Map<int, int list> = 
+        startingNumbers
+        |> Array.indexed
+        |> Array.fold (fun map (idx, item) -> 
+            map
+            |> Map.change item (function 
+                | None -> Some [ idx ]
+                | Some l -> Some (idx :: l)
+            ) 
+        ) Map.empty
+    let wasNumberSeenBeforeLastTurn (number : int) (currentTurn : int) = 
+        match Map.tryFind number occurrencesMap with
+        | None -> false
+        | Some [] -> false
+        | Some l -> 
+            let occurrencesBeforeGivenTurn = 
+                List.filter (fun o -> o < (currentTurn - 1)) l
+            in
+            not (List.isEmpty occurrencesBeforeGivenTurn)
+    in
+    let markNumberOccurrence (number : int) (index : int) =
+        occurrencesMap <- 
+            occurrencesMap
+            |> Map.change number (function
+                | None -> Some [ index ]
+                | Some l -> Some [index; l.Head]
+            )
+    let speak (lastSpokenNumber : int) (index : int) =
         let currentNumber = 
-            if not (Array.contains lastNumber numbersUpToThatPoint) then
+            if not (wasNumberSeenBeforeLastTurn lastSpokenNumber index) then
                 0
             else
-                let idx1 = Array.findIndexBack ((=) lastNumber) alreadySpokenNumbers in
-                let subArrayUpToThatPoint = alreadySpokenNumbers.[0..(idx1 - 1)] in
-                let idx2 = Array.findIndexBack ((=) lastNumber) subArrayUpToThatPoint in
-                idx1 - idx2 
+                let [idx2;idx1] = 
+                    occurrencesMap
+                    |> Map.find lastSpokenNumber 
+                in
+                idx2 - idx1
         in
-        Array.append alreadySpokenNumbers [|currentNumber|]
+        markNumberOccurrence currentNumber index
+        currentNumber
     in
-    let mutable startingList = startingNumbers in
-    for _ in startingNumbers.Length .. 2019 do 
-        startingList <- speak startingList 
-    startingList.[2019] // Arrays are 0-indexed, so the 2020th word spoken is at index 2019
-
-let part2 (input : string) =
-    "not yet implemented!"
-
+    let mutable lastNumber = 18
+    for idx in startingNumbers.Length .. 2019 do 
+        lastNumber <- speak lastNumber idx
+    let part1Solution = lastNumber
+    for idx in 2020 .. 29999999 do 
+        lastNumber <- speak lastNumber idx
+    (part1Solution, lastNumber)
+    
 [<EntryPoint>]
 let main _ =
     let startingNumbers = [|9;12;1;4;17;0;18|] in
-    printfn "Day 15A: %d" (part1 startingNumbers)
-//    printfn "Day 15B: %s" (part2 input)
+    let part1Solution, part2Solution = solve startingNumbers in
+    printfn "Day 15A: %d" part1Solution
+    printfn "Day 15B: %d" part2Solution
     0
